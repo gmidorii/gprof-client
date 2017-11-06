a87de0c9a567f80b80667af1802a3e998fc81f83
tagasouichiro <stitow14@gmail.com>
2017-11-06 23:38:46 +0900

    Add struct

---
+++ graphql.go
@@ -0,0 +1,31 @@
+package main
+
+type Prof struct {
+	Data struct {
+		CPU struct {
+			CacheSize int `json:"cache_size"`
+			Cores     []struct {
+				Percent float64 `json:"percent"`
+			} `json:"cores"`
+			Model     string `json:"model"`
+			ModelName string `json:"model_name"`
+		} `json:"cpu"`
+		Disk struct {
+			Io struct {
+				ReadCount int `json:"read_count"`
+			} `json:"io"`
+			Usage struct {
+				Free        int     `json:"free"`
+				Path        string  `json:"path"`
+				Total       int     `json:"total"`
+				Used        int     `json:"used"`
+				UsedPercent float64 `json:"used_percent"`
+			} `json:"usage"`
+		} `json:"disk" gq:"disk(path: $path)"`
+		File struct {
+			Content     string `json:"content"`
+			Name        string `json:"name"`
+			UpdatedTime string `json:"updated_time"`
+		} `json:"file" gq:"file(path: $path, num: $num)"`
+	} `json:"data"`
+}
