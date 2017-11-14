78227c213bf109647a87a804c4d68a48f7f6592a
tagasouichiro <stitow14@gmail.com>
2017-11-14 23:36:43 +0900

    Fix disk usage display

--- prof/disk/disk.go
+++ prof/disk/disk.go
@@ -1,6 +1,8 @@
 package disk
 
 import (
+	"log"
+
 	ui "github.com/gizak/termui"
 	"github.com/midorigreen/gprof-client/prof"
 )
@@ -14,23 +16,32 @@ func CreateWidget() *DiskWidget {
 }
 
 func (d *DiskWidget) Update(prof prof.Prof) {
-	disk := prof.Data.Disk
-	bar, ok := d.Widget.(*ui.BarChart)
-	if !ok {
-		return
-	}
-	bar.Data = []int{disk.Usage.Total, disk.Usage.Used, disk.Usage.Free}
+	//disk := prof.Data.Disk
+	//bar, ok := d.Widget.(*ui.BarChart)
+	//if !ok {
+	//	return
+	//}
+	//bar.Data = []int{disk.Usage.Total, disk.Usage.Used, disk.Usage.Free}
 }
 
 func (d *DiskWidget) Create(prof prof.Prof) []ui.GridBufferer {
 	disk := prof.Data.Disk
-	bar := ui.NewBarChart()
-	bar.DataLabels = []string{"T", "U", "F"}
-	bar.BorderLabel = "Disk memory"
-	bar.Data = []int{disk.Usage.Total, disk.Usage.Used, disk.Usage.Free}
-	bar.Height = 10
-	bar.Width = 40
+	log.Println(prof.Data.Disk)
+	mbar := ui.NewMBarChart()
+	mbar.Data[0] = []int{mbToGB(disk.Usage.Used), mbToGB(disk.Usage.Total)}
+	mbar.Data[1] = []int{mbToGB(disk.Usage.Free), 0}
+	mbar.DataLabels = []string{"memory", "test"}
+	mbar.Width = 50
+	mbar.Height = 10
+	mbar.BarWidth = 20
+	mbar.BarGap = 10
+	mbar.BarColor[0] = ui.ColorBlue
+	mbar.BarColor[1] = ui.ColorYellow
 
-	d.Widget = bar
+	d.Widget = mbar
 	return []ui.GridBufferer{d.Widget}
+}
+
+func mbToGB(b int) int {
+	return b / 1024
 }
